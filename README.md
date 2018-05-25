97f505231b24cf3b275144bda7bdb45ca5e45832
Hudson, Philip <Philip.Hudson@zuhlke.com>
2018-05-25 11:44:43 +0100

    fix: parse json

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# sre-jenkins
+# Site Reliability Topic Team - Jenkins Configurator
+
+You should run 
+```
+helm init
+```
+first.
+
+
+Installation of new Jenkins:
+```
+./install-jenkins.sh
+```
--- build-jenkinsfile.groovy
+++ build-jenkinsfile.groovy
@@ -3,6 +3,7 @@ final def region = "eu-west-2"
 final def imageName = "sre-camp18"
 
 def releaseVersion = ""
+def imageTag = ""
 
 podTemplate(label: label,
         containers: [containerTemplate(name: 'docker', image: 'docker', command: 'cat', ttyEnabled: true)],
@@ -10,16 +11,15 @@ podTemplate(label: label,
 
     node(label) {
         container('docker') {
-            stage('Checkout') {
+            stage('Initialise') {
                 parallel(
-                    checkout: {
+                    "Checkout SCM": {
                         checkout([$class: 'GitSCM', branches: [[name: '*/master']], userRemoteConfigs: [[url: 'https://github.com/disco-funk/sre-microservice']]])
-                        final def jsonFile = file('version.json')
-                        final def parsedJson = new groovy.json.JsonSlurper().parseText(jsonFile.text)
+                        final def parsedJson = readJSON file: './version.json'
                         final def snapshotVersion = parsedJson.version
                         releaseVersion = snapshotVersion.replace('0-SNAPSHOT', env.BUILD_NUMBER)
                     },
-                    installJDK: {
+                    "Install JDK": {
                         sh 'apk --update add openjdk8'
                     }
                 )
@@ -27,15 +27,18 @@ podTemplate(label: label,
             }
 
             stage('Build Binary') {
-                sh "./gradlew -PbuildNum=${releaseVersion} build"
+                sh "./gradlew -PreleaseVersion=${releaseVersion} build"
+            }
+
+            stage('Build Docker Image') {
+                withCredentials([string(credentialsId: 'aws_account_number', variable: 'awsAccountNumber')]) {
+                    imageTag = "${awsAccountNumber}.dkr.ecr.${region}.amazonaws.com/${imageName}:${releaseVersion}"
+                    sh "docker build  --build-arg RELEASE_VERSION=${releaseVersion} -t ${imageTag} ."
+                }
             }
 
             stage('Push to AWS ECR') {
                 withCredentials([string(credentialsId: 'aws_account_number', variable: 'awsAccountNumber')]) {
-                    def imageTag = "${awsAccountNumber}.dkr.ecr.${region}.amazonaws.com/${imageName}:${releaseVersion}"
-                    sh "docker build -t ${imageTag} ."
-                    sh "docker image ls"
-
                     withAWS(credentials: 'aws_credentials') {
                         sh ecrLogin()
                         sh "docker push ${imageTag}"
