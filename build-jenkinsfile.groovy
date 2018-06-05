dc05eb37d7e0f4b945dbb54618f118ce1205692a
Hudson, Philip <Philip.Hudson@zuhlke.com>
2018-06-05 16:22:16 +0200

    feat: push helm to new repo

--- build-jenkinsfile.groovy
+++ build-jenkinsfile.groovy
@@ -61,7 +61,7 @@ podTemplate(label: label,
                         sh "sed -i 's/1.0.0-SNAPSHOT/${releaseVersion}/g' ${baseDir}/sre-helm/sre/values.yaml"
                         sh "helm package --version=${releaseVersion} ${baseDir}/sre-helm/sre"
                         sh "mv ${baseDir}/sre-${releaseVersion}.tgz ${baseDir}/sre-helm-repo/docs/"
-                        sh "cd ${baseDir}/sre/helm-repo && git remote set-url origin https://disco-funk:${githubToken}@github.com/disco-funk/sre-helm-repo.git"
+                        sh "cd ${baseDir}/sre-helm-repo && git remote set-url origin https://disco-funk:${githubToken}@github.com/disco-funk/sre-helm-repo.git"
                         sh "git add ${baseDir}/sre-helm-repo/docs/sre-${releaseVersion}.tgz && git commit -m 'Jenkins automated push - new helm package version ${releaseVersion}' && git push origin HEAD:master"
                     }
                 }
