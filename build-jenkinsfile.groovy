376d82acb96d0492b51c4ca5cdb930efe3e532c1
Hudson, Philip <Philip.Hudson@zuhlke.com>
2018-06-05 15:24:41 +0200

    feat: push helm to new repo

--- build-jenkinsfile.groovy
+++ build-jenkinsfile.groovy
@@ -56,21 +56,16 @@ podTemplate(label: label,
                         sh "apk update && apk add git && helm init --upgrade"
                         sh "git config --global user.email 'man@themoon.com'"
                         sh "git config --global user.name 'Helm User'"
-                        sh 'mkdir /home/jenkins/workspace/sre-build/sre-helm'
-                        sh 'mkdir /home/jenkins/workspace/sre-build/sre-helm-repo'
+//                        sh 'mkdir /home/jenkins/workspace/sre-build/sre-helm'
+//                        sh 'mkdir /home/jenkins/workspace/sre-build/sre-helm-repo'
+
+                        sh "git clone https://disco-funk:${githubToken}@github.com/disco-funk/sre-helm.git"
                         sh 'cd /home/jenkins/workspace/sre-build/sre-helm'
-                        git(
-                            url: "https://disco-funk:${githubToken}@github.com/disco-funk/sre-helm.git",
-                            credentialsId: 'github'
-                        )
                         sh "ls -la"
                         sh 'sed -i \'s/1.0.0-SNAPSHOT/' + releaseVersion + '/g\' /home/jenkins/workspace/sre-build/sre-helm/sre/values.yaml'
                         sh 'helm package --version=' + releaseVersion + ' /home/jenkins/workspace/sre-build/sre-helm/sre'
+                        sh "git clone https://disco-funk:${githubToken}@github.com/disco-funk/sre-helm-repo.git"
                         sh 'cd /home/jenkins/workspace/sre-build/sre-helm-repo'
-                        git(
-                                url: "https://disco-funk:${githubToken}@github.com/disco-funk/sre-helm-repo.git",
-                                credentialsId: 'github'
-                        )
                         sh 'mv /home/jenkins/workspace/sre-build/sre-helm/sre-' + releaseVersion + '.tgz /home/jenkins/workspace/sre-build/sre-helm-repo/docs/'
                         sh 'git add /home/jenkins/workspace/sre-build/sre-helm-repo/docs/sre-' + releaseVersion + '.tgz'
                         sh "git commit -m 'Jenkins automated push - new helm package version ${releaseVersion}'"
