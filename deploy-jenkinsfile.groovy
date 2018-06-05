6b031409743929a3cd559286d2bd19290336902f
Hudson, Philip <Philip.Hudson@zuhlke.com>
2018-06-05 12:04:02 +0200

    feat: replace snapshot version

--- deploy-jenkinsfile.groovy
+++ deploy-jenkinsfile.groovy
@@ -3,7 +3,7 @@ final def region = "eu-west-2"
 final def imageName = "sre-camp18"
 
 def releaseVersion = "1.0.9"
-def helmPackageVersion = "0.1.4"
+def helmPackageVersion = "0.1.5"
 def imageTag = ""
 
 podTemplate(label: label,
