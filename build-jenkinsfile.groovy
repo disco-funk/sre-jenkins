1cf5d8ddbd68c82af430bf6f24d274724c76920d
Hudson, Philip <Philip.Hudson@zuhlke.com>
2018-06-05 12:31:32 +0200

    feat: missing podTemplates

--- build-jenkinsfile.groovy
+++ build-jenkinsfile.groovy
@@ -6,7 +6,9 @@ def releaseVersion = ""
 def imageTag = ""
 
 podTemplate(label: label,
-        containers: [containerTemplate(name: 'docker', image: 'docker', command: 'cat', ttyEnabled: true)],
+        containers: [containerTemplate(name: 'docker', image: 'docker', command: 'cat', ttyEnabled: true),
+                     containerTemplate(name: 'helm', image: 'lachlanevenson/k8s-helm:latest', command: 'cat', ttyEnabled: true),
+                     containerTemplate(name: 'kubectl', image: 'lachlanevenson/k8s-kubectl:v1.8.8', command: 'cat', ttyEnabled: true)],
         volumes: [hostPathVolume(mountPath: '/var/run/docker.sock', hostPath: '/var/run/docker.sock')]) {
 
     node(label) {
