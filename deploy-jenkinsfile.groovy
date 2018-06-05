5f4bb7a9f458f1b8d0129fa1d6833e394b8d52eb
Hudson, Philip <Philip.Hudson@zuhlke.com>
2018-06-05 10:41:37 +0200

    feat: helm package & install

--- deploy-jenkinsfile.groovy
+++ deploy-jenkinsfile.groovy
@@ -33,8 +33,8 @@ podTemplate(label: label,
                 withCredentials([string(credentialsId: 'aws_account_number', variable: 'awsAccountNumber')]) {
                     sh "apk update && apk add git && helm init --upgrade"
                     sh "git clone https://github.com/disco-funk/sre-helm.git && cd sre-helm && ls ./sre -la && pwd"
-                    sh 'helm package $(pwd)/sre'
-                    sh 'helm install $(pwd)/sre-0.1.1.tgz'
+                    sh 'helm package $(pwd)/sre-helm/sre'
+                    sh 'helm install $(pwd)/sre-helm/sre-0.1.1.tgz'
                 }
             }
         }
